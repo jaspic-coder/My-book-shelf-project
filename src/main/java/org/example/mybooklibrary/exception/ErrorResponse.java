39584b385defc3afadc199982297bc33e3eafc23
Gloria <mutuyegloria@gmail.com>
2025-07-05 02:21:37 -0700

    add the changes of exception

--- src/main/java/org/example/mybooklibrary/exception/ErrorResponse.java
+++ src/main/java/org/example/mybooklibrary/exception/ErrorResponse.java
@@ -1,7 +1,15 @@
 package org.example.mybooklibrary.exception;
 
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timestamp;
     private int status;
@@ -9,13 +17,4 @@ public class ErrorResponse {
     private String message;
     private String path;
 
-    public ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
-    // Getters and setters omitted for brevity
 }
