3d985f377a4792664a2387d7b9784955605a38b2
Gloria <mutuyegloria@gmail.com>
2025-07-10 06:27:02 -0700

    add

---
+++ src/main/java/org/example/mybooklibrary/exception/InvalidPasswordException.java
@@ -0,0 +1,12 @@
+package org.example.mybooklibrary.exception;
+
+
+
+
+    public class Invalid extends RuntimeException {
+        public InvalidPasswordException(String message) {
+            super(message);
+        }
+    }
+
+
